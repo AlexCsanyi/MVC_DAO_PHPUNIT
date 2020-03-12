c1e694b57d80e24883c9453ba6db667621642f34
AlexCsanyi <alex.csanyi@gmail.com>
2020-03-12 05:29:24 +0000

    installed Mockery and started implementing tests

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,31 @@
+<?php 
+    class UserTest extends \PHPUnit\Framework\TestCase
+    {
+        public function testThatWeCanGetTheFirstName()
+        {
+            $user = new \App\Models\User;
+
+            $user->setFirstName('Billy');
+
+            $this->assertEquals($user->getFirstName(), 'Billy');
+        }
+
+        public function testThatWeCanGetTheLastName()
+        {
+            $user = new \App\Models\User;
+
+            $user->setLastName('Smith');
+
+            $this->assertEquals($user->getLastName(), 'Smith');
+        }
+
+        public function testFullNameIsReturned()
+        {
+            $user = new \App\Models\User;
+            $user->setFirstName('Billy');
+            $user->setLastName('Smith');
+
+            $this->assertEquals($user->getFullName(), 'Billy Smith');
+        }
+    }
+?>
